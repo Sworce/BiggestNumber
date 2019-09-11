8f2778545349831626987e3cf9342ccee9ac2f14
Sworce <Sworce>
2019-09-11 16:14:07 -0700

    Added user input and took the bigger number between the two

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,23 +1,33 @@
-//Author:
+//Author:Tommy Luo
 #include<iostream>
-
+using namespace std;
 int main()
 {
   //declare variables here
+  int num1 = 0;
+  int num2 = 0;
 
-  std::cout<<"Please enter a whole number:\n";
+  cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
-  std::cout<<"Please enter another whole number:\n";
+  cin >> num1;
+  
+  cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  cin >> num2;
   //determine if first or second is bigger and store that in a third variable
+ 
+ int bigger;
+ if (num1 > num2) {
+    bigger = num1;
+  }
+  else {
+    bigger = num2;
+  }
 
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+  cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
-
-  std::cout<<std::endl<<"Thank you for playing.\n";
+  cout << bigger << endl;
+  cout<<"Thank you for playing.\n";
   
   return 0;
 }
